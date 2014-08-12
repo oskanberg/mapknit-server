7fdb1afc15a5ec7de9dd5bc91c145c477506e013
Oliver Skanberg-Tippen <ost500@york.ac.uk>
2014-08-12 23:10:29 +0100

    basic peer requesting & fallback

--- CCServer.js
+++ CCServer.js
@@ -1,94 +1,104 @@
-
-define(function () {
+define(['Util'], function(Util) {
 
     function CCServer() {
 
-        function async(fn) {
-            setTimeout(function() {
-                fn();
-            }, 0);
-        }
+        // function async(fn) {
+        //     setTimeout(function() {
+        //         fn();
+        //     }, 0);
+        // }
 
         var self = this;
-        self.ws = new WebSocket('ws://localhost:1123', 'rp0');
-        self.ws.onopen = function () {
-            self.isOpen = true;
+        self.ws = new WebSocket('ws://localhost:1123');
+        self.ws.onerror = function() {
+            Util.debug('aw shite, error', 'error');
         };
-        self.messageRegister = {};
-        self.onmessage = function (event) {
-            var msg = JSON.parse(event.data);
-            var subs = messageRegister[msg.type];
-            for (var i = 0; i < subs; i++) {
-                async(function() {
-                    subs[i](msg);
-                });
-            }
-        }
-    }
-
-    CCServer.prototype._uuid() = function() {
-        return (((1+Math.random())*0x10000)|0).toString(16).substring(1)
-        + '-' +(((1+Math.random())*0x10000)|0).toString(16).substring(1);
-    };
-
-    CCServer.prototype.getResourcePeers = function(url) {
-        var self = this;
-        var uuid = self._uuid();
-        var msg = {
-            type : 'peerrequest',
-            requestId : uuid,
-            resourceId : atob(url)
+        self.ws.onclose = function() {
+            Util.debug('close :(', 'error');
         };
-        var promise = new Promise(function (resolve, reject) {
-            this.messageRegister['peerresponse'].push(function (message) {
-                if (message.requestId == uuid) {
-                    resolve(message.peers);
-                }
-            });
+        self.connected = new Promise(function(resolve, reject) {
+            self.ws.onopen = function() {
+                Util.debug('connected to server', 'log');
+                resolve();
+            };
         });
-        this.ws.send(JSON.stringify(msg));
+
+        self.messageRegister = {};
+        self.ws.onmessage = function(event) {
+            Util.debug('got message from server', 'log');
+            var msg = JSON.parse(event.data);
+            var subs = self.messageRegister[msg.type];
+            console.log(typeof subs[0]);
+            for (var i = 0; i < subs.length; i++) {
+                // async(function() {
+                    subs[i](msg);
+                // });
+            }
+        };
     }
+
 
     CCServer.prototype.getResourcePeersXMLHTTP = function(url) {
         var self = this;
         // TODO
-        if (!self.isOpen) return;
-        var promise = new Promise(function(resolve, reject) {
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function () {
-                if (xmlhttp.readystate == 4) {
-                    if (xmlhttp.status == 200) {
-                        response = JSON.parse(xmlhttp.responseText);
-                        resolve(response.peers);
-                    } else {
-                        // TODO : fail gracefully
-                        reject(Error(':( could not get resource peers'));
+        Util.debug('not yet implemented', 'log');
+        return;
+        self.connected.then(function() {
+            var promise = new Promise(function(resolve, reject) {
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function() {
+                    if (xmlhttp.readystate == 4) {
+                        if (xmlhttp.status == 200) {
+                            response = JSON.parse(xmlhttp.responseText);
+                            resolve(response.peers);
+                        } else {
+                            // TODO : fail gracefully
+                            reject(Error(':( could not get resource peers'));
+                        }
                     }
-                }
-            };
-            var url = self.CC_SERV + '/rp/' + btoa(url);
-            xmlhttp.open('GET', url, true);
+                };
+                var url = self.CC_SERV + '/rp/' + btoa(url);
+                xmlhttp.open('GET', url, true);
+            });
+            return promise;
         });
-        return promise;
     };
 
     CCServer.prototype.getResourcePeers = function(url) {
         var self = this;
-        var uuid = self._uuid();
-        var msg = {
-            type : 'peerrequest',
-            requestId : uuid,
-            resourceId : atob(url)
-        };
-        var promise = new Promise(function (resolve, reject) {
-            this.messageRegister['peerresponse'].push(function (message) {
-                if (message.requestId == uuid) {
-                    resolve(message.peers);
-                }
+        var promise = new Promise(function(resolve, reject) {
+            self.connected.then(function() {
+                var uuid = self._uuid();
+                var msg = {
+                    type: 'peerrequest',
+                    requestId: uuid,
+                    resourceId: btoa(url)
+                };
+
+                // register for response
+                self._oneTimeSubscribe('peerresponse', function(message) {
+                    if (message.requestId == uuid) {
+                        Util.debug('got peers', 'log');
+                        resolve(message.peers);
+                    } else {
+                        Util.debug(':o(', 'log');
+                    }
+                });
+                self.ws.send(JSON.stringify(msg));
             });
         });
-        this.ws.send(JSON.stringify(msg));
-    }
+        return promise;
+    };
+
+    CCServer.prototype._uuid = function() {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16);
+    };
+
+    CCServer.prototype._oneTimeSubscribe = function(type, fn) {
+        var self = this;
+        self.messageRegister[type] = self.messageRegister[type] || [];
+        self.messageRegister[type].push(fn);
+    };
 
     return CCServer;
 });
--- mapknit.js
+++ mapknit.js
@@ -1,13 +1,15 @@
-
 require.config({
     paths: {
-        'peerjs' : 'bower_components/peerjs/peer'
+        'peerjs': 'bower_components/peerjs/peer'
     }
 });
 
-require(['ImageUtils', 'CCServ', 'peerjs', 'shims'], function(ImageUtils, CCServ) {
+require(['ImageUtils', 'CCServer', 'Util', 'peerjs', 'shims'], function(ImageUtils, CCServ, Util) {
 
     var ATTRIBUTE_NAME = 'data-src';
+
+    var iu = new ImageUtils();
+    var cc = new CCServ();
 
     function getLoadCandidates(tag) {
         var imageElements = document.getElementsByTagName(tag);
@@ -15,26 +17,25 @@ require(['ImageUtils', 'CCServ', 'peerjs', 'shims'], function(ImageUtils, CCServ
         for (var i = 0; i < imageElements.length; i++) {
             if (imageElements[i].getAttribute(ATTRIBUTE_NAME) !== null) {
                 loadCandidates.push({
-                    el : imageElements[i],
-                    src : imageElements[i].getAttribute(ATTRIBUTE_NAME);
+                    el: imageElements[i],
+                    src: imageElements[i].getAttribute(ATTRIBUTE_NAME)
                 });
             }
         }
         return loadCandidates;
     }
 
-    var iu = new ImageUtils();
-    var cc = new CCServ();
-
-    var imageCandidates = getImageCandidates('img');
+    var imageCandidates = getLoadCandidates('img');
 
     for (var i = 0; i < imageCandidates.length; i++) {
-        cc.getResourcePeers(imageCandidates[i].src)
+        var ic = imageCandidates[i];
+        cc.getResourcePeers(ic.src)
             .then(function(candidates) {
-                console.log(candidates);
+                // console.log(candidates);
                 if (candidates.length == 0) {
                     // fall back to regular loading
-                    imageCandidates[i].el.src = imageCandidates[i].src;
+                    console.log(ic);
+                    ic.el.src = ic.src;
                 }
             });
     }
